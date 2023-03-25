ade5011f807b0310217be4dc634a51ddeb82a243
Kevin Kuch <kuchkevin96@gmail.com>
2023-03-25 11:58:00 -0400

    Ajout nav bar

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Mon Portefolio
-## Ce portefolio présente mes réalisations dans le domaine de développement Web. J'ai développé ces projets en utilisant HTML, CSS, JS et React.
---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState} from "react";
+import "./Header.css";
+
+export default function Header() {
+    const [toggle, setToggle] = useState(false);
+
+
+  return (
+    <header className="Header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Kuch</a>
+
+        <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
+            <ul className="nav__list grid">
+                <li className="nav__item">
+                    <a href="#home" className="nav__link active-link">
+                        <i className="uil uil-estate nav__icon"></i> Home
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#about" className="nav__link">
+                        <i className="uil uil-user nav__icon"></i> About
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#skill" className="nav__link">
+                        <i className="uil uil-file-alt nav__icon"></i> Skills
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#portefolio" className="nav__link">
+                        <i className="uil uil-scenery nav__icon"></i> Portefolio
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#experiences" className="nav__link">
+                        <i className="uil uil-briefcase-alt nav__icon"></i> Experiences
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#experiences" className="nav__link">
+                    <i className="uil uil-graduation-cap nav__icon"></i> Education
+                    </a>
+                </li>
+            </ul>
+            
+            <i className="uil uil-times nav__close" onClick={() => setToggle (!toggle)}></i>
+        </div>
+        <div className="nav__toggle" onClick={() => setToggle (!toggle)}>
+            <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+}
