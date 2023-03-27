e5f7d36e2c8bdca182823ee562e6041d77c76198
Kevin Kuch <kuchkevin96@gmail.com>
2023-03-27 06:32:32 -0400

    Full home page done

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Social() {
+
+
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/kevin-kuch/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-linkedin"></i>
+            </a>
+            <a href="https://github.com/KevinKuch" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-github"></i>
+            </a>
+        </div>
+    );
+}
