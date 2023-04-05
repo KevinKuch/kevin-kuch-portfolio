3506a62eb1580926627d15ad15ee62bbcc1e90fd
Kevin Kuch <kuchkevin96@gmail.com>
2023-04-05 09:49:43 -0400

    fix a tag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,13 +9,11 @@ export default function Footer({}) {
         <div className='Footer' id='footer'>
             <div className='footer__social'>
                 <a href='https://www.linkedin.com/in/kevin-kuch/' className="footer__social-icon" target='_blank' rel='noreferrer'>
-                    {/* with unicons */}
                     <i className='uil uil-linkedin'></i>
-                    {/* github */}
-                    <a href="https://github.com/KevinKuch" className="footer__social-icon" target='_blank' rel='noreferrer'>
+                </a>
+                <a href="https://github.com/KevinKuch" className="footer__social-icon" target='_blank' rel='noreferrer'>
                     <i className='uil uil-github'></i>
                     </a>
-                </a>
             </div>
             <div className="copyright">
                 <p>Tous droits réservés © {year} Kevin Kuch</p>
